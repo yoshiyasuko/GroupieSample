364fb341506682f6bfa862e9dc3ed391d3195b04
Yoshiyasu <te108128@gmail.com>
2019-03-14 01:17:32 +0900

    implement: android_hello_view

--- app/src/main/java/jp/egg/fried/groupiesample/activity/MainActivity.kt
+++ app/src/main/java/jp/egg/fried/groupiesample/activity/MainActivity.kt
@@ -2,11 +2,19 @@ package jp.egg.fried.groupiesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //RecyclerViewの初期化
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView
+
     }
+
+
 }
